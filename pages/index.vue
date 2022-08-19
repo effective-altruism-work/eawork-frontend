f414be157b6d66e455782e2e56ca2f1fc9ecc346
Victor Yunenko <victor@givemomentum.com>
2022-08-18 22:32:46 -0700

    add jobs rendering from api

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,16 @@
-<script setup lang="ts">
+<script
+  setup
+  lang="ts"
+>
+const { data } = await useFetch("https://backend.eawork.org/api/jobs");
 </script>
 
 <template>
   <div>
-    EA Work
+    <h1>EA Work</h1>
+
+    <ul>
+      <li v-for="job in data">{{ job.title }}</li>
+    </ul>
   </div>
 </template>
