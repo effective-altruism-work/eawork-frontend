13b05d75ae4981866fb20c789f6ab4982b94ce90
Victor Yunenko <victor@givemomentum.com>
2022-08-22 21:40:47 -0700

    create-alert-btn: add incomplete http query

--- src/components/create-alert-btn.vue
+++ src/components/create-alert-btn.vue
@@ -1,27 +1,50 @@
 <script setup lang="ts">
 
+import { useRuntimeConfig } from "#app";
 import { CBox, CFlex, CButton, CInput, CText } from "@chakra-ui/vue-next";
+import axios from "axios";
 import { ref } from "vue";
 import { config } from "~/theme/config";
 
 const props = defineProps<{ query: any; queryRaw: string }>();
+const nuxtConfig = useRuntimeConfig();
 
-const isShowModal = ref(false);
+const state = {
+  isShowModal: ref(false),
+  email: ref(""),
+  isSubmitting: ref(false),
+  isSuccess: ref(null),
+  isError: ref(false),
+}
 
-const submitted = ref(false)
-const submitHandler = async () => {
-  // Let's pretend this is an ajax request:
-  await new Promise((r) => setTimeout(r, 1000))
-  submitted.value = true
+async function createJobAlert() {
+  state.isSubmitting.value = true;
+  state.isSuccess.value = null;
+  state.isError.value = false;
+  try {
+    const res = await axios.post(`${nuxtConfig.public.apiBase}/jobs/subscribe/`, {
+      email: state.email.value,
+      query: props.query,
+      queryRaw: props.queryRaw,
+    });
+    if (res.data.success) {
+      state.isSuccess.value = true;
+    } else {
+      state.isError.value = true;
+    }
+  } catch (e) {
+    state.isError.value = true;
+  }
+  state.isSubmitting.value = false;
 }
 
 </script>
 
 <template>
-  <CButton color-scheme="blue" @click="isShowModal = true">Create Alert</CButton>
+  <CButton color-scheme="blue" @click="state.isShowModal.value = true">Create Alert</CButton>
 
   <VueFinalModal
-    v-model="isShowModal"
+    v-model="state.isShowModal.value"
     :lock-scroll="false"
     :click-to-close="true"
     :esc-to-close="true"
@@ -45,8 +68,16 @@ const submitHandler = async () => {
         <CText w="fit-content">Receive emails for your current search query.</CText>
         
       </CFlex>
-      <CInput type="email" placeholder="joe@example.com"/>
-      <CButton mt="-3" max-w="fit-content" color-scheme="blue">Subscribe</CButton>
+      <CInput v-model="state.email.value" type="email" placeholder="joe@example.com"/>
+      <CButton
+        @click="createJobAlert()"
+        :is-loading="state.isSubmitting.value"
+        mt="-3"
+        max-w="fit-content"
+        color-scheme="blue"
+      >
+        Subscribe
+      </CButton>
       <CText mt="-0.5" w="fit-content" color="gray.400" font-size="sm">You can unsubscribe anytime.</CText>
     </CFlex>
   </VueFinalModal>
