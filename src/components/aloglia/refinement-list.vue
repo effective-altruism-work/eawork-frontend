7fba5ae117429f7cc9a5916b4830fff7f3dbe8a0
Victor Yunenko <victor@givemomentum.com>
2022-08-22 19:24:00 -0700

    refinement-list: hide search if disabled

--- src/components/aloglia/refinement-list.vue
+++ src/components/aloglia/refinement-list.vue
@@ -21,7 +21,7 @@ const props = defineProps<{ label: string; attribute: string }>();
           sendEvent,
         }"
       >
-        <CInput size="sm" @input="searchForItems($event.currentTarget.value)" />
+        <CInput v-if="isFromSearch" size="sm" @input="searchForItems($event.currentTarget.value)" />
         <ul>
           <li v-if="isFromSearch && !items.length">No results.</li>
           <li v-for="item in items" :key="item.value">
