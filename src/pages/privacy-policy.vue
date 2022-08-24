bbb55951764133c8a96c5fa7febbed4e1dfd0b81
Victor Yunenko <victor@givemomentum.com>
2022-08-24 15:37:13 -0700

    add privacy-policy and terms-of-service

---
+++ src/pages/privacy-policy.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { CHeading, CText } from "@chakra-ui/vue-next";
+
+</script>
+
+<template>
+  <CHeading>Privacy Policy</CHeading>
+  <CText>Provided as it is.</CText>
+</template>
---
+++ src/pages/terms-of-service.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { CHeading, CText } from "@chakra-ui/vue-next";
+
+</script>
+
+<template>
+  <CHeading>Terms of Service</CHeading>
+  <CText>Provided as it is.</CText>
+</template>
