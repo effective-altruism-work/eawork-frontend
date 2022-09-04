c06e11fee189e62dbfd89bc106ff2895c3244f13
Victor Yunenko <victor@givemomentum.com>
2022-09-03 21:11:44 -0700

    add numeric-menu chakra-ui styles

---
+++ src/components/aloglia/numeric-menu.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { CHeading, CBox } from "@chakra-ui/vue-next";
+
+const props = defineProps<{
+  attribute: string;
+  label: string;
+  items: Array<any>;
+}>();
+
+</script>
+
+<template>
+  <CBox mt="6">
+    <CHeading size="sm" mb="2px" font-weight="normal">{{ props.label }}</CHeading>
+    <ais-numeric-menu
+      :attribute="props.attribute"
+      :items="props.items"
+    />
+  </CBox>
+</template>
+
+<style lang="scss">
+@import "~/styles/chakra-ui.scss";
+
+.ais-NumericMenu-label {
+  @extend .chakra-radio;
+
+  .ais-NumericMenu-labelText {
+    margin-left: 0.5rem;
+  }
+}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,7 +6,7 @@ import { subDays, startOfYear, getUnixTime, endOfYear, addDays } from "date-fns"
 import { OhVueIcon } from "oh-vue-icons";
 import { ref } from "vue";
 import CurrentRefinements from "~/components/aloglia/current-refinements.vue";
-import ExperienceRangeInput from "~/components/aloglia/experience-range-input.vue";
+import NumericMenu from "~/components/aloglia/numeric-menu.vue";
 import RefinementList from "~/components/aloglia/refinement-list.vue";
 import SearchBox from "~/components/aloglia/search-box.vue";
 import BtnJobsAlert from "~/components/btn-jobs-alert.vue";
@@ -112,35 +112,31 @@ function saveQueryJson(state: {
           <RefinementList attribute="tags_skill" label="Skills" />
           <RefinementList attribute="tags_generic" label="Tags" />
 
-          <CBox>
-            <CHeading size="sm" :mt="space">Posted at</CHeading>
-            <ais-numeric-menu
-              attribute="posted_at"
-              :items="[
-                { label: 'Anytime' },
-                { label: 'Today', start: getUnixTime(subDays(new Date(), 1)) },
-                { label: 'A week ago', start: getUnixTime(subDays(new Date(), 7)) },
-                { label: '30d ago', start: getUnixTime(subDays(new Date(), 30)) },
-                { label: '3m ago', start: getUnixTime(subDays(new Date(), 91)) },
-                { label: 'This year', start: getUnixTime(startOfYear(new Date())) },
-              ]"
-            />
-          </CBox>
-
-          <CBox :mt="space">
-            <CHeading size="sm">Closes in</CHeading>
-            <ais-numeric-menu
-              attribute="closes_at"
-              :items="[
-                { label: 'Anytime' },
-                { label: 'Today', end: getUnixTime(addDays(new Date(), 1)) },
-                { label: 'In a week', end: getUnixTime(addDays(new Date(), 7)) },
-                { label: 'In 30d', end: getUnixTime(addDays(new Date(), 30)) },
-                { label: 'In 3m', end: getUnixTime(addDays(new Date(), 91)) },
-                { label: 'This year', end: getUnixTime(endOfYear(new Date())) },
-              ]"
-            />
-          </CBox>
+          <NumericMenu
+            label="Posted at"
+            attribute="posted_at"
+            :items="[
+              { label: 'Anytime' },
+              { label: 'Today', start: getUnixTime(subDays(new Date(), 1)) },
+              { label: 'A week ago', start: getUnixTime(subDays(new Date(), 7)) },
+              { label: '30d ago', start: getUnixTime(subDays(new Date(), 30)) },
+              { label: '3m ago', start: getUnixTime(subDays(new Date(), 91)) },
+              { label: 'This year', start: getUnixTime(startOfYear(new Date())) },
+            ]"
+          />
+          
+          <NumericMenu
+            label="Closes in"
+            attribute="closes_at"
+            :items="[
+              { label: 'Anytime' },
+              { label: 'Today', end: getUnixTime(addDays(new Date(), 1)) },
+              { label: 'In a week', end: getUnixTime(addDays(new Date(), 7)) },
+              { label: 'In 30d', end: getUnixTime(addDays(new Date(), 30)) },
+              { label: 'In 3m', end: getUnixTime(addDays(new Date(), 91)) },
+              { label: 'This year', end: getUnixTime(endOfYear(new Date())) },
+            ]"
+          />
         </CFlex>
 
         <CFlex direction="column" min-w="69%">
@@ -189,3 +185,7 @@ function saveQueryJson(state: {
     </AisInstantSearch>
   </CBox>
 </template>
+
+<style lang="scss">
+@import "~/styles/chakra-ui.scss";
+</style>
