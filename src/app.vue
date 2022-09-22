03348f7cdedb5d588cf31f465f4490b3afe931d3
Victor Yunenko <victor@givemomentum.com>
2022-09-21 22:24:59 -0700

    patch SEO meta render

--- src/app.vue
+++ src/app.vue
@@ -15,6 +15,19 @@ const state = {
 useHead({
   titleTemplate: (titleChunk) => (titleChunk ? `${titleChunk} - 80,000 hours` : "80,000 hours"),
   link: [{ rel: "icon", type: "image/svg+xml", href: "/favicon.ico" }],
+  meta: [
+    { name: "og:title", content: "80,000 Hours Job Board" },
+    { name: "og:description", content: "A curated list of the most promising vacancies we're aware of. If you’re a good fit for one of these, it could be your best opportunity to help solve one of the world’s most pressing problems." },
+    { name: "og:image", content: "https://80000hours.org/wp-content/uploads/2017/03/job_board_share_logos3.png" },
+    { name: "og:image:width", content: "1200" },
+    { name: "og:image:height", content: "630" },
+    { name: "og:url", content: "https://jobs.80000hours.org/" },
+    { name: "og:site_name", content: "80,000 Hours" },
+    { name: "article:publisher", content: "https://www.facebook.com/80000Hours" },
+    { name: "twitter:card", content: "summary_large_image" },
+    { name: "twitter:description", content: "If you’re a good fit for one of these roles, it could be your best opportunity to help solve one of the world’s most pressing problems." },
+    { name: "twitter:site", content: "@80000hours" },
+  ],
 });
 
 </script>
