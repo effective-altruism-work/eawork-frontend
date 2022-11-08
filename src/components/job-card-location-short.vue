1f1ffdbda3d4279b42f4f07cdb0015ff4c84e19d
Conor Barnes <conorbarnes93@gmail.com>
2022-11-08 14:44:58 +0000

    better location list in cards

--- src/components/job-card-location-short.vue
+++ src/components/job-card-location-short.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { CFlex, CText } from "@chakra-ui/vue-next";
+import { CFlex, CText, CBox } from "@chakra-ui/vue-next";
 import { OhVueIcon } from "oh-vue-icons";
 import { strings } from "~/constants";
 import { useComp } from "~/utils/structs";
@@ -9,10 +9,14 @@ const props = defineProps<{
   job: JobAlgolia;
 }>();
 
-const countriesWithRemotes = computed(() => {
+const remotesAndMaybeCountries = computed(() => {
   const remoteLocations = props.job.tags_location_80k.filter((location) =>
     location.includes("Remote"),
   );
+
+  if (props.job.tags_city.length) {
+    return remoteLocations;
+  }
 
   const remoteLocationsBare = remoteLocations.map((location) =>
     location.replace("Remote, ", ""),
@@ -32,12 +36,8 @@ const comp = useComp(() => {
     ? props.job.tags_location_80k.find((s) => s.toLowerCase().includes("remote"))
     : "";
 
-  const showCity =
-    props.job.tags_city.length && !props.job.tags_city.includes(strings.remoteLiteral);
-
   return {
     remoteLocation,
-    isShouldShowCity: showCity,
     cities: props.job.tags_city.map((city) =>
       city.replace("Washington, DC metro area", "Washington, DC"),
     ),
@@ -49,6 +49,7 @@ const comp = useComp(() => {
   <CFlex
     align="center"
     gap="1"
+    w="80%"
     v-if="props.job.tags_city.length || props.job.tags_country.length"
   >
     <OhVueIcon
@@ -58,19 +59,32 @@ const comp = useComp(() => {
       style="margin-bottom: 1px"
     />
 
-    <CFlex :no-of-lines="1">
-      <CFlex display="inline" v-if="comp.isShouldShowCity" align="center" color="#9BADB6">
+    <CFlex
+      color="#9BADB6"
+      flex-wrap="nowrap"
+      align-items="center"
+      flex-direction="row"
+      w="100%"
+    >
+      <CFlex display="inline" v-if="props.job.tags_city.length" align="center">
         <CText display="inline" v-for="(city, index) of comp.cities" :key="city"
           ><CText mx="2" display="inline" v-if="index">▪</CText>{{ city }}</CText
         >
       </CFlex>
 
-      <CFlex
-        v-if="!job.tags_city.length && countriesWithRemotes.length > 0"
-        align="center"
-        color="#9BADB6"
+      <CText
+        display="inline"
+        v-if="job.tags_city.length && remotesAndMaybeCountries.length"
+        mx="2"
+        >▪</CText
       >
-        <CText v-for="(country, index) in countriesWithRemotes" :key="country">
+
+      <CFlex v-if="remotesAndMaybeCountries.length" display="inline" align-items="center">
+        <CText
+          display="inline"
+          v-for="(country, index) in remotesAndMaybeCountries"
+          :key="country"
+        >
           <CText mx="2" display="inline" v-if="index">▪</CText>{{ country }}</CText
         >
       </CFlex>
