a9a7a029f29d31bda851ddb22e35d7d47e41336a
Conor Barnes <conorbarnes93@gmail.com>
2022-11-17 15:23:53 -0400

    improve search ui

--- src/components/algolia/debounced-searchbox.vue
+++ src/components/algolia/debounced-searchbox.vue
@@ -57,11 +57,12 @@ export default {
     <input
       type="search"
       placeholder="Keywords"
-      class="w-full text-gray-900 pl-12 p-4 h-14 rounded-xl bg-[#e8ecee] placeholder-[#3a4142] hover:border-2 hover:bg-[#eef2f5] active:border-2 focus:border-2 active:bg-gray-50 focus:bg-gray-50 outline-none"
+      class="w-full text-gray-900 pl-12 p-4 h-14 rounded-xl bg-[#e8ecee] placeholder-gray-500 hover:outline outline-gray-300 hover:bg-[#eef2f5] active:outline focus:outline active:bg-gray-50 focus:bg-gray-50"
       v-model="query"
     />
     <OhVueIcon
       id="icon"
+      scale="1.2"
       class="absolute self-center left-4 mt-[1px]"
       w="6"
       h="6"
